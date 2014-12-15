84dea08a9c0597ae0959062a4c0c1f8808e22db7
luismasuelli-trescloud <luis.masuelli@trescloud.com>
2014-12-15 14:48:35 -0500

    sexto cantrip: leer por consola (hasta cumplir una condicion). leer por consola un solo caracter. leer una opción (de un solo caracter) por consola.

---
+++ cantrips/console/getch.py
@@ -0,0 +1,68 @@
+import six
+
+
+class _Getch:
+    """Gets a single character from standard input.  Does not echo to the screen."""
+    def __init__(self):
+        try:
+            self.impl = _GetchWindows()
+        except ImportError:
+            self.impl = _GetchUnix()
+
+    def __call__(self): return self.impl()
+
+
+class _GetchUnix:
+    def __init__(self):
+        import tty, sys
+
+    def __call__(self):
+        import sys, tty, termios
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+class _GetchWindows:
+    def __init__(self):
+        import msvcrt
+
+    def __call__(self):
+        import msvcrt
+        return msvcrt.getch()
+
+
+getch = _Getch()
+igetch = lambda: getch().lower()
+
+
+def input(message, until, single_char=False, transform=lambda a: a):
+    """
+    Keeps asking for input (each time in a new line) until a condition
+        over the input text evaluates to true. Returns the input. This
+        input can be done using text input or character input.
+    """
+    def _getch(message):
+        print message,
+        return getch()
+
+    while True:
+        text = transform(six.input(message) if not single_char else _getch(message))
+        if until(text):
+            return text
+
+
+def input_option(message, options="yn", error_message=None):
+    """
+    Reads an option from the screen, with a specified prompt.
+        Keeps asking until a valid option is sent by the user.
+    """
+    def _valid(character):
+        if character not in options:
+            print error_message % character
+    return input("%s [%s]" % (message, options), _valid, True, lambda a: a.lower())
