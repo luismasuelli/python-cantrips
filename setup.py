1440faec4cef694cf21a1da08a314004bdcd422b
luismasuelli <luismasuelli@hotmail.com>
2014-12-16 19:33:24 -0500

    cambio de version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-cantrips',
-    version='0.3.1',
+    version='0.3.2',
     packages=['cantrips', 'cantrips.watch'],
     url='https://github.com/luismasuelli/python-cantrips',
     license='LGPL',
