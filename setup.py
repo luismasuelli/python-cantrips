28158b7bbd9ce9762360e6a7ce5cf295f23a6606
luismasuelli <luismasuelli@hotmail.com>
2015-01-28 23:20:52 -0500

    version 0.5.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='python-cantrips',
-    version='0.5.2',
+    version='0.5.3',
     packages=['cantrips',
               'cantrips.watch',
               'cantrips.types',
