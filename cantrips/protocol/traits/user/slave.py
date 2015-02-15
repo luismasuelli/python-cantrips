88df370174f2e52ff309e6ee2f0f01482a118cd6
luismasuelli <luismasuelli@hotmail.com>
2015-02-14 19:42:22 -0500

    algunas funciones fueron puestas

--- cantrips/protocol/traits/user/slave.py
+++ cantrips/protocol/traits/user/slave.py
@@ -1,12 +1,41 @@
 from base import UserBroadcast
 from cantrips.patterns.actions import AccessControlledAction
 from cantrips.protocol.traits.decorators.authcheck import IAuthCheck
+from cantrips.protocol.traits.provider import IProtocolProvider
 
 
-class UserSlaveBroadcast(UserBroadcast, IAuthCheck):
+class UserSlaveBroadcast(UserBroadcast, IAuthCheck, IProtocolProvider):
     """
     This broadcast adds an existing user. It does not support login features.
     """
+
+    CHANNEL_RESPONSE_NS = 'notify'
+    CHANNEL_RESPONSE_CODE_RESPONSE = 'response'
+
+    CHANNEL_NS = 'channel'
+    CHANNEL_CODE_JOIN = 'join'
+    CHANNEL_CODE_JOINED = 'joined'
+    CHANNEL_CODE_PART = 'part'
+    CHANNEL_CODE_PARTED = 'parted'
+
+    CHANNEL_RESULT_ALLOW_JOIN = 'join-accepted'
+    CHANNEL_RESULT_DENY_JOIN = 'join-rejected'
+    CHANNEL_RESULT_ALLOW_PART = 'part-accepted'
+    CHANNEL_RESULT_DENY_PART = 'part-rejected'
+
+    @classmethod
+    def specification(cls):
+        return {
+            cls.CHANNEL_NS: {
+                cls.CHANNEL_CODE_JOIN: 'server',
+                cls.CHANNEL_CODE_PART: 'server',
+                cls.CHANNEL_CODE_JOINED: 'client',
+                cls.CHANNEL_CODE_PARTED: 'client',
+            },
+            cls.CHANNEL_RESPONSE_NS: {
+                cls.CHANNEL_RESPONSE_CODE_RESPONSE: 'client'
+            }
+        }
 
     def __init__(self, key, master, *args, **kwargs):
         super(UserSlaveBroadcast, self).__init__(key, master=master, *args, **kwargs)
@@ -26,6 +55,12 @@ class UserSlaveBroadcast(UserBroadcast, IAuthCheck):
     def force_part(self, user, *args, **kwargs):
         """
         Forces a user to be removed from the slave.
+        """
+        pass
+
+    def force_join(self, user, *args, **kwargs):
+        """
+        Forces a user to be added to the slave.
         """
         pass
 
@@ -49,36 +84,36 @@ class UserSlaveBroadcast(UserBroadcast, IAuthCheck):
 
     def _command_is_allowed_join(self, socket):
         """
-
+        Determines whether the current socket is allowed to join the slave.
         """
-        pass
+        return self._result_deny(self.CHANNEL_RESULT_DENY_JOIN)
 
-    def _command_accepted_join(self, socket):
-        """
-
-        """
-        pass
-
-    def _command_rejected_join(self, socket):
+    def _command_accepted_join(self, result, socket):
         """
 
         """
         pass
+
+    def _command_rejected_join(self, result, socket):
+        """
+        The join command was rejected.
+        """
+        socket.send_message(self.CHANNEL_RESPONSE_NS, self.CHANNEL_RESPONSE_CODE_RESPONSE, result=result, channel=self.key)
 
     def _command_is_allowed_part(self, socket):
         """
-
+        Determines whether the current socket is allowed to leave the slave.
         """
-        pass
+        return self._result_deny(self.CHANNEL_RESULT_DENY_PART)
 
-    def _command_accepted_part(self, socket):
-        """
-
-        """
-        pass
-
-    def _command_rejected_part(self, socket):
+    def _command_accepted_part(self, result, socket):
         """
 
         """
         pass
+
+    def _command_rejected_part(self, result, socket):
+        """
+        The leave command was rejected.
+        """
+        socket.send_message(self.CHANNEL_RESPONSE_NS, self.CHANNEL_RESPONSE_CODE_RESPONSE, result=result, channel=self.key)
