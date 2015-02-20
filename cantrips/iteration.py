60e9c7b9c5b682448f16e2c50681a15a58c69e2a
luismasuelli-trescloud <luis.masuelli@trescloud.com>
2015-02-20 14:16:45 -0500

    vigesimonoveno cantrip: forzar iterable

--- cantrips/iteration.py
+++ cantrips/iteration.py
@@ -10,3 +10,16 @@ def items(iterable):
         return (p for p in iterable.items())
     else:
         return (p for p in enumerate(iterable))
+
+
+def iterable(value):
+    """
+    If the value is not iterable, we convert it to an iterable containing
+      that only value.
+    :param x:
+    :return:
+    """
+    try:
+        return iter(value)
+    except TypeError:
+        return value,
